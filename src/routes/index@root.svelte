27ffb7090533fc02bd1aa5abb001c3d1f2eb622a
IloveNooodles <mgarebaldhie80@gmail.com>
2022-05-07 15:50:01 +0700

    refactor

--- src/routes/index@root.svelte
+++ src/routes/index@root.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+	import { onMount } from 'svelte/internal';
+	onMount(() => {
+		document.body.style.backgroundColor = '#F1F5F8';
+	});
+</script>
+
 <div class="home">
-  <h1>Welcome to gare's svelte + ts project</h1>
+	<h1>Welcome to gare's svelte + ts project</h1>
 </div>
